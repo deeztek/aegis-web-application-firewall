611740c5b579be5152bd7fcce7e4771e9be447d9
deeztek <dino.edwards@mydirectmail.net>
2020-08-26 06:07:04 -0400

    Added nginx reload after certbot renewal in /etc/letsencrypt/cli.ini

--- ubuntu_aegis_waf_update.sh
+++ ubuntu_aegis_waf_update.sh
@@ -1,5 +1,5 @@
 SCRIPTPATH=$(pwd)
 /bin/cp -r $SCRIPTPATH/dirstructure/opt/aegis-waf/* /opt/aegis-waf && \
 /bin/chmod +x /opt/aegis-waf/scripts/*.sh && \
-/bin/cp $SCRIPTPATH/dirstructure/etc/letsencrypt/cli.ini /etc/letsencrypt/cli.ini && \
+/bin/grep -qxF 'deploy-hook = systemctl reload nginx' /etc/letsencrypt/cli.ini || /bin/cp $SCRIPTPATH/dirstructure/etc/letsencrypt/cli.ini /etc/letsencrypt/cli.ini && \
 echo "Aegis WAF Update Complete"
